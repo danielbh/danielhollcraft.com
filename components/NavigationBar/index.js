2f773e1c8a79bee31bd86aa270e9ff92fe60e1fc
Daniel Hollcraft <daniel@hollcraft.com>
2017-04-08 19:03:13 +0200

    styling and fixing bug in nav container showing active link incorrectly

--- components/NavigationBar/index.js
+++ components/NavigationBar/index.js
@@ -5,10 +5,9 @@ import React, {
   PropTypes,
 } from 'react';
 import { config } from 'config'
-
 import { prefixLink } from 'gatsby-helpers'; // eslint-disable-line
 import FontAwesome from 'react-fontawesome';
-import { Link } from 'react-router'; // eslint-disable-line
+import { IndexLink } from 'react-router'; // eslint-disable-line
 
 import {
   Navbar,
@@ -17,7 +16,7 @@ import {
   Image
 } from 'react-bootstrap'
 
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
 
 import './index.scss'
 
@@ -26,33 +25,39 @@ const NavigationBar = (props) => {
     <Navbar className="navbar"  collapseOnSelect>
       <Navbar.Header>
         <Navbar.Brand>
-            <Link to='/'>
+            <IndexLink to="/">
                 <Image
                   className='logo'
                   src={require('../../images/logo.png')}
                   alt="daniel_hollcraft_logo"
                 />
-            </Link>
+            </IndexLink>
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav pullRight
              className="nav-links">
-          <LinkContainer to='/'>
+          <IndexLinkContainer to="/">
             <NavItem eventKey={1}>
-              <FontAwesome name="bookmark" className='icon'/>
+              <FontAwesome name="pencil" className='icon'/>
               Blog
             </NavItem>
-          </LinkContainer>
-          <LinkContainer to={prefixLink('/projects/')}>
+          </IndexLinkContainer>
+          <LinkContainer to={prefixLink('/services/')}>
             <NavItem eventKey={2}>
               <FontAwesome name="rocket" className='icon' />
-              Projects
+              Services
+            </NavItem>
+          </LinkContainer>
+          <LinkContainer to="/portfolio/">
+            <NavItem eventKey={3}>
+              <FontAwesome name="cubes" className='icon' />
+              Portfolio
             </NavItem>
           </LinkContainer>
           <LinkContainer to="/contact/">
-            <NavItem eventKey={3}>
+            <NavItem eventKey={4}>
               <FontAwesome name="envelope" className='icon' />
               Contact
             </NavItem>
