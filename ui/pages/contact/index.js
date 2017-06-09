3ed15071ea0df7ea743be18f98816f74bfde2338
Daniel Hollcraft <daniel@hollcraft.com>
2017-06-09 13:43:05 +0200

    contact form validation

--- ui/pages/contact/index.js
+++ ui/pages/contact/index.js
@@ -4,9 +4,11 @@
 
 import React, {
   PropTypes,
+  Component,
 } from 'react';
 
-import Helmet from "react-helmet"
+import Helmet from 'react-helmet'
+
 import {
   Col,
   Row,
@@ -21,60 +23,127 @@ import { config } from 'config'
 
 import './index.scss'
 
-const Contact = (props) => {
-  return (
-    <div className="contact-wrapper">
-      <Helmet
-        title={config.siteTitle}
-        meta={[
-          {"name": "description", "content": "Blog"},
-          {"name": "keywords", "content": "sample, something"},
-        ]}
-      />
-      <Grid>
-        <Row>
-          <Col xs={12} sm={12} md={12} lg={12} >
-            <div className="contact">
-              <Form horizontal>
-                <FormGroup controlId="formHorizontalEmail">
-                  <Col componentClass={ControlLabel} sm={2}>
-                    Email
-                  </Col>
-                  <Col sm={10}>
-                    <FormControl type="email" placeholder="Email" />
-                  </Col>
-                </FormGroup>
-                <FormGroup controlId="formHorizontalName">
-                  <Col componentClass={ControlLabel} sm={2}>
-                    Name
-                  </Col>
-                  <Col sm={10}>
-                    <FormControl type="text" placeholder="Name" />
-                  </Col>
-                </FormGroup>
-                <FormGroup controlId="formHorizontalMessage">
-                  <Col componentClass={ControlLabel} sm={2}>
-                    Message
-                  </Col>
-                  <Col sm={10}>
-                    <FormControl componentClass="textarea" placeholder="Your message..." />
-                  </Col>
-                </FormGroup>
-                <FormGroup>
-                  <Col smOffset={2} sm={10}>
-                    <Button bsStyle="success" bsSize="large" type="submit">
-                      Send
-                    </Button>
-                  </Col>
-                </FormGroup>
-              </Form>
-            </div>
-          </Col>
-        </Row>
-      </Grid>
-    </div>
-  );
-};
+class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitPress: false,
+      email: '',
+      name: '',
+      message: ''
+    };
+  }
+
+  submit (evt) {
+    evt.preventDefault();
+    this.setState({
+      submitPressed: true
+    });
+
+    if(this.isFormValid()) {
+      console.log("can submit form")
+    }
+  }
+
+  handleEmailChange(evt) {
+    this.setState({email: evt.target.value});
+  }
+
+  handleNameChange(evt) {
+    this.setState({name: evt.target.value});
+  }
+
+  handleMessageChange(evt) {
+    this.setState({message: evt.target.value});
+  }
+
+  isValidEmail() {
+     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.state.email);
+  }
+
+  getEmailValidationState() {
+    return (!this.isValidEmail() || this.state.email === '') && this.state.submitPressed ? 'error' : null
+  }
+
+  getTextFieldValidationState(fieldValue) { return fieldValue === '' && this.state.submitPressed ? 'error' : null }
+
+  isFormValid() {
+    return this.state.message !== "" && this.state.name !== "" && this.state.email !== "" && this.isValidEmail()
+  }
+
+  render() {
+    return (
+      <div className="contact-wrapper">
+        <Helmet
+          title={config.siteTitle}
+          meta={[
+            {"name": "description", "content": "Blog"},
+            {"name": "keywords", "content": "sample, something"},
+          ]}
+        />
+        <Grid>
+          <Row>
+            <Col xs={12} sm={12} md={12} lg={12} >
+              <div className="contact">
+                <Form horizontal>
+                  <FormGroup controlId="formHorizontalEmail" validationState={this.getEmailValidationState()}>
+                    <Col componentClass={ControlLabel} sm={2}>
+                      Email
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl
+                        type="email"
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={this.handleEmailChange.bind(this)}
+                      />
+                      <FormControl.Feedback />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup controlId="formHorizontalName"
+                             validationState={this.getTextFieldValidationState(this.state.name)}
+                  >
+                    <Col componentClass={ControlLabel} sm={2}>
+                      Name
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl type="text" placeholder="Name"
+                                   value={this.state.name}
+                                   onChange={this.handleNameChange.bind(this)}
+                      />
+                      <FormControl.Feedback />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup controlId="formHorizontalMessage"
+                             validationState={this.getTextFieldValidationState(this.state.message)}
+                  >
+                    <Col componentClass={ControlLabel} sm={2}>
+                      Message
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl componentClass="textarea" placeholder="Your message..."
+                                   value={this.state.message}
+                                   onChange={this.handleMessageChange.bind(this)} />
+                      <FormControl.Feedback />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup>
+                    <Col smOffset={2} sm={10}>
+                      <Button bsStyle="success" bsSize="large" type="submit" onClick={this.submit.bind(this)}>
+                        Send
+                      </Button>
+                    </Col>
+                  </FormGroup>
+                </Form>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 Contact.propTypes = {};
 Contact.defaultProps = {};
