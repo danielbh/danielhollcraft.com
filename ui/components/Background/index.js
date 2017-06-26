b4065a05330bd23dab169bc8d90b1eca74744a49
Daniel Hollcraft <daniel@hollcraft.com>
2017-06-26 11:59:53 +0200

    re-add trianglify

--- ui/components/Background/index.js
+++ ui/components/Background/index.js
@@ -8,58 +8,58 @@ import React, {
   Addons
 } from 'react';
 
-//import ReactDOM from 'react-dom'
-// import update from 'immutability-helper';
-// import Trianglify from 'trianglify';
-//import TrianglifyCanvas from './components/TrianglifyCanvas';
+import ReactDOM from 'react-dom'
+import update from 'immutability-helper';
+import Trianglify from 'trianglify';
+import TrianglifyCanvas from './components/TrianglifyCanvas';
 
 import './index.scss'
 
 export default class Background extends Component {
-  //
-  // constructor(props) {
-  //   super(props);
-  //   // Copy Trianglify.defaults instead of referencing it. Prevents big issues according to github.com/qrohlf
-  //   this.state = update(Trianglify.defaults, {});
-  //   this.state.resize_timer = null;
-  //   this.state.seed = "Daniel Hollcraft"; // Seed with my own name!
-  // }
-  //
-  // debounceResize() {
-  //   clearTimeout(this.state.resize_timer);
-  //   this.setState({resize_timer: setTimeout(this.handleResize.bind(this), 100)});
-  // }
-  //
-  // handleResize() {
-  //   this.resize()
-  // }
-  //
-  // componentDidMount() {
-  //   window.addEventListener('resize', this.debounceResize.bind(this));
-  //   this.resize()
-  // }
-  //
-  // resize() {
-  //   this.setState({
-  //     height: ReactDOM.findDOMNode(this).offsetHeight,
-  //     width: ReactDOM.findDOMNode(this).offsetWidth
-  //   });
-  // }
-  //
-  // componentWillUnmount() {
-  //   window.removeEventListener('resize', this.debounceResize.bind(this));
-  // }
+
+  constructor(props) {
+    super(props);
+    // Copy Trianglify.defaults instead of referencing it. Prevents big issues according to github.com/qrohlf
+    this.state = update(Trianglify.defaults, {});
+    this.state.resize_timer = null;
+    this.state.seed = "Daniel Hollcraft"; // Seed with my own name!
+  }
+
+  debounceResize() {
+    clearTimeout(this.state.resize_timer);
+    this.setState({resize_timer: setTimeout(this.handleResize.bind(this), 100)});
+  }
+
+  handleResize() {
+    this.resize()
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.debounceResize.bind(this));
+    this.resize()
+  }
+
+  resize() {
+    this.setState({
+      height: ReactDOM.findDOMNode(this).offsetHeight,
+      width: ReactDOM.findDOMNode(this).offsetWidth
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.debounceResize.bind(this));
+  }
 
   render() {
     return(
       <div className="background" style={{backgroundColor: this.props.color}}>
-        {/*<TrianglifyCanvas*/}
-          {/*height={this.state.height + 10}*/}
-          {/*width={this.state.width + 10}*/}
-          {/*x_colors="GnBu"*/}
-          {/*variance={0.90}*/}
-          {/*cell_size={60}*/}
-          {/*seed={this.state.seed}/>*/}
+        <TrianglifyCanvas
+          height={this.state.height + 10}
+          width={this.state.width + 10}
+          x_colors="GnBu"
+          variance={0.90}
+          cell_size={60}
+          seed={this.state.seed}/>
       </div>
     );
   }
