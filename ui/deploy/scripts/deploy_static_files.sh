f6d681f78964311357615b022f87062c330d60c3
Daniel Hollcraft <daniel@hollcraft.com>
2017-07-17 16:54:20 +0200

    add sitemap and google web master tools verification file to static deploy script

--- ui/deploy/scripts/deploy_static_files.sh
+++ ui/deploy/scripts/deploy_static_files.sh
@@ -17,6 +17,10 @@ WEBSITE_URL=$(aws \
     --query "Stacks[0].Outputs[?OutputKey=='WebSiteUrl'] | [0].OutputValue" \
     --output text)
 
+#Deploy site content
 aws s3 sync --acl 'public-read' --delete ui/public/ "s3://${BUCKET_NAME}/"
+
+# Deploy sitemap and google webmaster tools verification
+aws s3 sync --acl 'public-read' ui/deploy/google-webmaster-tools/ "s3://${BUCKET_NAME}/"
 
 echo "Bucket URL: ${WEBSITE_URL}"
