745c9d07ffd892023f2c7776a28c566967064a30
Daniel Hollcraft <danielhollcraft@gmail.com>
2019-08-12 08:25:42 -0400

    feat: update content

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -18,10 +18,18 @@ class Main extends Component {
           <article className="container">
             <div id="homepage-description">
               <h2>Web Developer</h2>
-              <h2>Frontend | React | Node.js</h2>
-              <h2 style={{ textDecoration: 'underline' }}>
-                <Link to="/portfolio">Check out my work</Link>
-              </h2>
+              <p>
+                Hello. My name is Daniel. I 've been working in the
+                software industry since 2008, and have been a professional
+                software developer since 2011. I've worked in e-learning,
+                agricultural monitoring, tourism, and fintech. My current focus
+                is building microservices in a cloud native environment.
+                In my spare time I enjoy experimenting and creating new and
+                exciting things with the Go Programming Language. I'm in the
+                middle of updating this website. For the time being if you want
+                to learn more about me, I recommend <a href="https://github.com/danielbh">checking out my github</a> or <a href="https://www.linkedin.com/in/daniel-hollcraft/">my linkedin profile</a>.
+
+              </p>
             </div>
           </article>
         </section>
