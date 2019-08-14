7f5cb40a84bb883bfedefbfc6b07e642be73d2d4
Daniel Hollcraft <danielhollcraft@gmail.com>
2019-08-14 07:12:29 -0400

    chore: minor deps and css fixes

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -19,14 +19,14 @@ class Main extends Component {
             <div id="homepage-description">
               <h2>Web Developer</h2>
               <p>
-                Hello. My name is Daniel. I 've been working in the
+                Hello. My name is Daniel. I've been working in the
                 software industry since 2008, and have been a professional
                 software developer since 2011. I've worked in e-learning,
                 agricultural monitoring, tourism, and fintech. My current focus
                 is building microservices in a cloud native environment.
                 In my spare time I enjoy experimenting and creating new and
                 exciting things with the Go Programming Language. I'm in the
-                middle of updating this website. For the time being if you want
+                middle of updating this website. For the time being, if you want
                 to learn more about me, I recommend <a href="https://github.com/danielbh">checking out my github</a> or <a href="https://www.linkedin.com/in/daniel-hollcraft/">my linkedin profile</a>.
 
               </p>
